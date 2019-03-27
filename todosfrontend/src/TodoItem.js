a60525f7455bfc53b627d98b3f63340d1497e5f5
Shubham Singh <shubham1998singh.ss@gmail.com>
2019-03-27 17:41:25 -0500

    Fixed submodules

---
+++ todosfrontend/src/TodoItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './TodoItem.css';
+
+const TodoItem = ({name, completed, onDelete, onToggle}) => (
+    <li>
+        <span 
+            style={{
+                textDecoration: completed ? 'line-through' : 'none'
+            }}
+            onClick={onToggle}
+        >
+            {name}
+        </span>
+        <span onClick={onDelete}> X </span>
+    </li>
+);
+
+export default TodoItem;
---
+++ todosfrontend/src/api.js
@@ -0,0 +1,111 @@
+const API_URL = '/api/todos/';
+
+export async function getTodos() {
+    return fetch(API_URL)
+        .then(resp => {
+            if (!resp.ok) {
+                if (resp.status >= 400 && resp.status < 500) {
+                    return resp.json().then(data => {
+                        let err = {
+                            errorMessage: data.message
+                        };
+                        throw err;
+                    });
+                } else {
+                    let err = {
+                        errorMessage: 'Please try again later. Server not responding.'
+                    };
+                    throw err;
+                }
+            }
+            return resp.json();
+        });
+}
+
+export async function createTodo(val) {
+    return fetch(API_URL, {
+            method: 'post',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+            }),
+            body: JSON.stringify({
+                name: val
+            })
+        })
+        .then(resp => {
+            if (!resp.ok) {
+                if (resp.status >= 400 && resp.status < 500) {
+                    return resp.json().then(data => {
+                        let err = {
+                            errorMessage: data.message
+                        };
+                        throw err;
+                    });
+                } else {
+                    let err = {
+                        errorMessage: 'Please try again later. Server not responding.'
+                    };
+                    throw err;
+                }
+            }
+            return resp.json();
+        })
+}
+
+export async function removeTodo(id) {
+    const deleteURL = API_URL + id;
+
+    return fetch(deleteURL, {
+            method: 'delete',
+        })
+        .then(resp => {
+            if (!resp.ok) {
+                if (resp.status >= 400 && resp.status < 500) {
+                    return resp.json().then(data => {
+                        let err = {
+                            errorMessage: data.message
+                        };
+                        throw err;
+                    });
+                } else {
+                    let err = {
+                        errorMessage: 'Please try again later. Server not responding.'
+                    };
+                    throw err;
+                }
+            }
+            return resp.json();
+        })
+}
+
+export async function updateTodo(todo) {
+    const updateURL = API_URL + todo._id;
+
+    return fetch(updateURL, {
+            method: 'put',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+            }),
+            body: JSON.stringify({
+                completed: !todo.completed
+            })
+        })
+        .then(resp => {
+            if (!resp.ok) {
+                if (resp.status >= 400 && resp.status < 500) {
+                    return resp.json().then(data => {
+                        let err = {
+                            errorMessage: data.message
+                        };
+                        throw err;
+                    });
+                } else {
+                    let err = {
+                        errorMessage: 'Please try again later. Server not responding.'
+                    };
+                    throw err;
+                }
+            }
+            return resp.json();
+        })
+}
